1786743af22de2426fffdbe73633e2ccb6f0624d
heldervasc <helder.vasc@e-2points.com>
2010-10-18 23:15:53 +0100

    dasdasd

--- Filters/Biquad/BiquadLowPass.cpp
+++ Filters/Biquad/BiquadLowPass.cpp
@@ -1,13 +1,30 @@
-/*
- *  BiquadLowPass.cpp
- *  Library
- *
- *  Created by Helder Vasconcelos on 10/10/11.
- *  Copyright 2010 __MyCompanyName__. All rights reserved.
- *
- */
-
-
+/************************************************************************
+ 
+ Product: Portable Audio Dsp Library 
+ Version: V0.0.1
+ File: HanningWindow.cpp
+ Creation date: 2010/08/10
+ Author: Hélder Vasconcelos helder.vasc@e-2points.com 
+ Description: Class Tester
+ 
+ 
+ Copyright (c) 2010 Hélder Vasconcelos
+ 
+ This library is free software; you can redistribute it and/or modify 
+ it under the terms of the GNU Lesser General Public License as published by 
+ the Free Software Foundation; either version 2.1 of the License, or 
+ (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful, 
+ but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License 
+ for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public License 
+ along with this library; if not, write to the Free Software Foundation, Inc., 
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ 
+ ****************************************************************************/
 
 #include "BiquadLowPass.h"
 
--- Filters/PoleFilter.h
+++ Filters/PoleFilter.h
@@ -1,12 +1,30 @@
-/*
- *  PoleFilter.h
- *  Library
- *
- *  Created by Helder Vasconcelos on 10/10/13.
- *  Copyright 2010 Parallel Universe. All rights reserved.
- *
- */
-
+/************************************************************************
+ 
+ Product: Portable Audio Dsp Library 
+ Version: V0.0.1
+ File: HanningWindow.cpp
+ Creation date: 2010/08/10
+ Author: Hélder Vasconcelos helder.vasc@e-2points.com 
+ Description: Class Tester
+ 
+ 
+ Copyright (c) 2010 Hélder Vasconcelos
+ 
+ This library is free software; you can redistribute it and/or modify 
+ it under the terms of the GNU Lesser General Public License as published by 
+ the Free Software Foundation; either version 2.1 of the License, or 
+ (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful, 
+ but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License 
+ for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public License 
+ along with this library; if not, write to the Free Software Foundation, Inc., 
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ 
+ ****************************************************************************/
 
 #ifndef POLEFILTER_H
 #define POLEFILTER_H
@@ -39,6 +57,8 @@ public:
 	
 	// Retrieve the zero-based i-th pole.
 	Complex    GetPole                ( int i );
+	
+	//tes
 
 	// Retrieve the zero-based i-th zero.
 	Complex    GetZero                ( int i );
@@ -123,97 +143,6 @@ protected:
 	
 	
 };
-
-
-
-
-template<class TFunction>
-Float32 BrentMinimizeCalc(
-						  TFunction& f,    // [in] objective function to minimize
-						  Float32 leftEnd,    // [in] smaller value of bracketing interval
-						  Float32 rightEnd,    // [in] larger value of bracketing interval
-						  Float32 epsilon,    // [in] stopping tolerance
-						  Float32& minLoc    // [out] location of minimum
-						  )
-{
-	Float32 d, e, m, p, q, r, tol, t2, u, v, w, fu, fv, fw, fx;
-	static const Float32 c = 0.5*(3.0 - ::sqrt(5.0));
-	static const Float32 SQRT_DBL_EPSILON = ::sqrt(DBL_EPSILON);
-	
-	Float32& a = leftEnd; Float32& b = rightEnd; Float32& x = minLoc;
-	
-	v = w = x = a + c*(b - a); d = e = 0.0;
-	fv = fw = fx = f(x);
-	int counter = 0;
-loop:
-	counter++;
-	m = 0.5*(a + b);
-	tol = SQRT_DBL_EPSILON*::fabs(x) + epsilon; t2 = 2.0*tol;
-	// Check stopping criteria
-	if (::fabs(x - m) > t2 - 0.5*(b - a))
-	{
-		p = q = r = 0.0;
-		if (::fabs(e) > tol)
-		{
-			// fit parabola
-			r = (x - w)*(fx - fv);
-			q = (x - v)*(fx - fw);
-			p = (x - v)*q - (x - w)*r;
-			q = 2.0*(q - r);
-			(q > 0.0) ? p = -p : q = -q;
-			r = e; e = d;
-		}
-		if (::fabs(p) < ::fabs(0.5*q*r) && p < q*(a - x) && p < q*(b - x))
-		{
-			// A parabolic interpolation step
-			d = p/q;
-			u = x + d;
-			// f must not be evaluated too close to a or b
-			if (u - a < t2 || b - u < t2)
-				d = (x < m) ? tol : -tol;
-		}
-		else
-		{
-			// A golden section step
-			e = (x < m) ? b : a;
-			e -= x;
-			d = c*e;
-		}
-		// f must not be evaluated too close to x
-		if (::fabs(d) >= tol)
-			u = x + d;
-		else if (d > 0.0)
-			u = x + tol;
-		else
-			u = x - tol;
-		fu = f(u);
-		// Update a, b, v, w, and x
-		if (fu <= fx)
-		{
-			(u < x) ? b = x : a = x;
-			v = w; fv = fw; 
-			w = x; fw = fx; 
-			x = u; fx = fu;
-		}
-		else
-		{
-			(u < x) ? a = u : b = u;
-			if (fu <= fw || w == x)
-			{
-				v = w; fv = fw; 
-				w = u; fw = fu;
-			}
-			else if (fu <= fv || v == x || v == w)
-			{
-				v = u; fv = fu;
-			}
-		}
-		goto loop;  // Yes, the dreaded goto statement. But the code
-		// here is faithful to Brent's orginal pseudocode.
-	}
-	return  fx;
-}
-
 
 
 #endif
